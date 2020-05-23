2470337f1d947eed7710b87cfa5476ce32a3fc8b
aplested <a.plested@gmail.com>
2020-05-23 17:47:39 +0200

    groupPeaksDialog working with test data

--- groupPeaksDialog.py
+++ groupPeaksDialog.py
@@ -1,6 +1,7 @@
 import sys
+import os.path
 from PySide2 import QtCore, QtGui
-from PySide2.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QPushButton, QLayout, QDialog, QLabel, QRadioButton, QVBoxLayout
+from PySide2.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QPushButton, QLayout, QDialog, QLabel, QRadioButton, QVBoxLayout, QFileDialog
 import numpy as np
 import pyqtgraph as pg
 import pandas as pd
@@ -10,10 +11,10 @@ class groupDialog(QDialog):
     def __init__(self, *args, **kwargs):
         super(groupDialog, self).__init__(*args, **kwargs)
         
-        #group = 5
-        self.peaksN = 50
-        
+        self.peaksN = 50    # it's a guess
+        self.dataLoaded = False
         self.makeDialog()
+        
     
     def makeDialog(self):
         """Create the controls for the dialog"""
@@ -34,14 +35,14 @@ class groupDialog(QDialog):
         self.N_ROI_label = QLabel('Grouping peaks from ROIs \n over sets')
         layout.addWidget(self.N_ROI_label, 2, 0, 1, 2)
         
-        groupNSB_selecter_label = QLabel('Number of grouped responses')
+        groupNSB_selecter_label = QLabel('Number of responses in group')
         layout.addWidget(groupNSB_selecter_label, 0, 0, 1, 2)
         
         # group N selecter
         self.groupNSB = pg.SpinBox(value=5, step = 1, bounds=[1, 10], delay=0)
         self.groupNSB.setFixedSize(60, 25)
         layout.addWidget(self.groupNSB, 0, 2, 1, 1)
-        self.groupNSB.valueChanged.connect(self.updateGroup)
+        self.groupNSB.valueChanged.connect(self.updateGrouping)
        
         _doScrapeBtn = QPushButton('Extract grouped data')
         _doScrapeBtn.clicked.connect(self.groupPeaks)
@@ -60,34 +61,35 @@ class groupDialog(QDialog):
         layout.addWidget(_doneBtn, row=6, col=2)
         self.setLayout(layout)
         
-        self.updateGroup()
+        self.updateGrouping()
     
     def saveGroupPeaks (self):
         print ("save group peak data")
         #print (self.hDF.df.head(5))
         
-        #format for header cells.
+        # format for header cells.
         self.hform = {
         'text_wrap': True,
         'valign': 'top',
-        'fg_color': '#D5D4AC',
+        'fg_color': '#A504AC',
         'border': 1}
         
         self.filename = QFileDialog.getSaveFileName(self,
-        "Save Peak Data", os.path.expanduser("~"))[0]
+        "Save Group Analysis", os.path.expanduser("~"))[0]
         
         #from XlsxWriter examples, John McNamara
         if self.filename:
             with pd.ExcelWriter(self.filename) as writer:
                 #save peaks into sheet
-                for _set in self.groupsextracted_by_set:
+                for _set, _df in self.groupsextracted_by_set.items():
                 
-                    _set.to_excel(writer, sheet_name=_set, startrow=1, header=False)
+                    _df.to_excel(writer, sheet_name=_set, startrow=1, header=False)
                     _workbook  = writer.book
                     _worksheet = writer.sheets[_set]
                     header_format = _workbook.add_format(self.hform)
-                    for col_num, value in enumerate(_set.columns.values):
-                        _worksheet.write(0, col_num + 1, value + _set, header_format)
+                    for col_num, value in enumerate(_df.columns.values):
+                        #print (col_num, value, _set)
+                        _worksheet.write(0, col_num + 1, str(value) + " " + _set, header_format)
     
     def setExternalParameters(self, extPa):
         """extPa is a dictionary of external parameters that can be passed"""
@@ -113,6 +115,7 @@ class groupDialog(QDialog):
         """Bring in external data for analysis"""
         
         self.peakData = data
+        print (self.peakData)
         pdk = self.peakData.keys()
         pdk_display = ", ".join(str(k) for k in pdk)
         N_ROI = [len (self.peakData[d].columns) for d in pdk]
@@ -122,36 +125,39 @@ class groupDialog(QDialog):
         _printable = "{}\n{}\n".format(pdk_display, [self.peakData[d].head() for d in pdk])
         print ("Added data of type {}:\n{}\n".format(type(self.peakData), _printable))
         
+        self.dataLoaded = True
+        self.updateGrouping()
         
     def groupPeaks(self):
         print ("Analayse group peaks.")
         
         self.prepGuiParameters()
         self.groupsextracted_by_set = {}
-        _step  = self.groupNSB.value()
+        _step  = int(self.groupNSB.value())
         
         for _set in self.peakData.keys():
             #prep means and sd frames
             c = self.peakData[_set].columns
-            
+            #print (c, _step)
             # make dataframes
-            _means = pd.dataframe([], range(_step), c)
-            _SDs = pd.dataframe([], range(_step), c)
+            _means = pd.DataFrame([], range(_step), c)
+            _SDs = pd.DataFrame([], range(_step), c)
             
             
             for p in range(_step):
-                #get pth row group
+                # get pth row group
                 _subset = self.peakData[_set][p::_step]
                 
-                _means[p] = _subset.describe.loc['mean']
-                _SDs[p] = _subset.describe.loc['std']
+                # each set of mean, sd results is assigned to a row
+                _means.iloc[p] = _subset.describe().loc['mean']
+                _SDs.iloc[p] = _subset.describe().loc['std']
             
             self.groupsextracted_by_set[_set + "_m"] = _means
             self.groupsextracted_by_set[_set + "_sd"] = _SDs
         
         # we can save now that we have data
-        self.saveGroupsBtn.state.setEnabled(True)
-    
+        self.saveGroupsBtn.setEnabled(True)
+        print (self.groupsextracted_by_set)
         """
         for _set in self.peakdata.keys():
             maxVal = len(self.tPeaks)
@@ -189,15 +195,23 @@ class groupDialog(QDialog):
         self.accept()"""
     
 
-    def updateGroup(self):
+    def updateGrouping(self):
         """recalculate groups"""
         
-        print ('update group')
+        print ('update grouping')
         
-        self.NRepeats  = self.peaksN / self.groupNSB.value()
+        if self.dataLoaded:
+            try:
+                _firstdf = list(self.peakData.values())[0]
+                print (_firstdf)
+                self.peaksN = _firstdf.shape[0]     # get the number of rows in the first DataFrame
+            except:
+                print ("couldn't find a dataframe in self.peakData")
+                
+        self.NRepeats  = int(self.peaksN / self.groupNSB.value())
         
         #update dialog
-        NrepeatsLabelText = "{0} peaks in each trace and so {1: .1f} repeats.".format(self.peaksN, self.NRepeats)
+        NrepeatsLabelText = "{0} peaks in each trace and so {1} repeats.".format(self.peaksN, self.NRepeats)
         self.NRepeatsLabel.setText(NrepeatsLabelText)
         
 
@@ -207,5 +221,18 @@ if __name__ == '__main__':
     
     app = QApplication([])
     main_window = groupDialog()
+    
+    # trial data
+    a = np.arange(150).reshape((50, 3))
+    b = np.arange(150).reshape((50, 3))
+    np.random.shuffle(b)
+    c = ['a', 'b', 'c']
+    
+    d = {
+        "Set One" : pd.DataFrame(a**2, columns=c),
+        "Set Two" : pd.DataFrame(b, columns=c)
+        }
+    main_window.addData(d)
+        
     main_window.show()
     sys.exit(app.exec_())
